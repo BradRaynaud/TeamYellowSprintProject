77af4cca8b3c9b8ecc7934417248dae6d8dbf453
Zachary Cloutet <Zcloutet@users.noreply.github.com>
2019-11-06 18:02:32 -0600

    Add files via upload

--- Implementation/GenericRPG/Properties/Resources1.Designer.cs
+++ Implementation/GenericRPG/Properties/Resources1.Designer.cs
@@ -63,6 +63,16 @@ namespace GenericRPG.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap abilityselectbackround {
+            get {
+                object obj = ResourceManager.GetObject("abilityselectbackround", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap animalcrossinggrass {
             get {
                 object obj = ResourceManager.GetObject("animalcrossinggrass", resourceCulture);
