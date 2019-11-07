090fef4ebda9fdba31cf304cb6add2f773f6ff54
attmesh <48996180+attmesh@users.noreply.github.com>
2019-11-07 08:04:52 -0600

    Add files via upload
    Form1 and  WindowsFormsApp1.Sln for the menus

---
+++ Implementation/GenericRPG/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Frm_Menu : Form
+    {
+        public Frm_Menu()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RPG_title_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_quitgame_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(1);
+        }
+    }
+}
