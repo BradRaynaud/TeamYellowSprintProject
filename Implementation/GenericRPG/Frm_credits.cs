469615b6f0266430d17dacdba0283e30a4348a4c
attmesh <48996180+attmesh@users.noreply.github.com>
2019-11-07 08:58:56 -0600

    Add files via upload
    Form credit menus

---
+++ Implementation/GenericRPG/Frm_credits.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Frm_credits : Form
+    {
+        public Frm_credits()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Frm_credits_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Menu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Credit_Label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Btn_Menu_Click_1(object sender, EventArgs e)
+        {
+            
+            
+        }
+    }
+}
